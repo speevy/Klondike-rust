6f9daa625f05f037bb1b457f19b00e4f48f69131
Ivan Lorca <ivan@ivan.cat>
2021-10-22 19:44:14 +0200

    Initial implementation of the game logic

--- src/card.rs
+++ src/card.rs
@@ -312,7 +312,7 @@ pub enum CardHolder {
 }
 
 pub struct Klondike<'a> {
-    deck: Deck,
+    deck: Box<Deck>,
     piles: Vec<Pile>,
     foundations: Vec<Foundation>,
     mover: &'a mut (dyn CardMover + 'a),
@@ -338,7 +338,7 @@ impl<'a> Klondike<'a> {
         Klondike {
             piles,
             foundations,
-            deck: Deck::init(&cards[card_idx..].to_vec()),
+            deck: Box::new(Deck::init(&cards[card_idx..].to_vec())),
             mover,
         }
     }
@@ -362,10 +362,15 @@ impl<'a> Klondike<'a> {
         match destination {
             CardHolder::FOUNDATION(dest_idx) => match origin {
                 CardHolder::FOUNDATION(origin_idx) => {
+                    // Both Origin and Destination are Foundations
+                    // First do some sanitary checks
                     let elements = self.foundations.len() as u32;
                     if origin_idx == dest_idx || elements < origin_idx || elements < dest_idx {
                         return false;
                     }
+
+                    // Now whe have to split the vector in order to extract 
+                    // the two mutable elements safely
                     let (origin, destination) = extract_two_mutable_elements(
                         &mut self.foundations,
                         origin_idx as usize,
@@ -383,7 +388,7 @@ impl<'a> Klondike<'a> {
                 }
                 CardHolder::DECK => {
                     return self.mover.move_cards(
-                        &mut self.deck,
+                        &mut *self.deck,
                         &mut self.foundations[dest_idx as usize],
                         number as usize,
                     );
@@ -398,10 +403,15 @@ impl<'a> Klondike<'a> {
                     );
                 }
                 CardHolder::PILE(origin_idx) => {
+                    // Both Origin and Destination are Piles
+                    // First do some sanitary checks
                     let elements = self.piles.len() as u32;
                     if origin_idx == dest_idx || elements < origin_idx || elements < dest_idx {
                         return false;
                     }
+
+                    // Now whe have to split the vector in order to extract the 
+                    // two mutable elements safely
                     let (origin, destination) = extract_two_mutable_elements(
                         &mut self.piles,
                         origin_idx as usize,
@@ -411,7 +421,7 @@ impl<'a> Klondike<'a> {
                 }
                 CardHolder::DECK => {
                     return self.mover.move_cards(
-                        &mut self.deck,
+                        &mut *self.deck,
                         &mut self.piles[dest_idx as usize],
                         number as usize,
                     );
@@ -421,6 +431,10 @@ impl<'a> Klondike<'a> {
                 return false;
             }
         }
+    }
+
+    pub fn take(&mut self) {
+        (*(self.deck)).take();
     }
 }
 
@@ -1147,26 +1161,26 @@ mod tests {
 
     #[test]
     fn test_extract_two_mutables() {
-        test_extract_two_mutables_case(0,1);
-        test_extract_two_mutables_case(1,0);
-        test_extract_two_mutables_case(2,3);
-        test_extract_two_mutables_case(3,2);
-        test_extract_two_mutables_case(4,7);
-        test_extract_two_mutables_case(7,4);
-        test_extract_two_mutables_case(7,9);
-        test_extract_two_mutables_case(9,7);
-        test_extract_two_mutables_case(8,9);
-        test_extract_two_mutables_case(9,8);
+        test_extract_two_mutables_case(0, 1);
+        test_extract_two_mutables_case(1, 0);
+        test_extract_two_mutables_case(2, 3);
+        test_extract_two_mutables_case(3, 2);
+        test_extract_two_mutables_case(4, 7);
+        test_extract_two_mutables_case(7, 4);
+        test_extract_two_mutables_case(7, 9);
+        test_extract_two_mutables_case(9, 7);
+        test_extract_two_mutables_case(8, 9);
+        test_extract_two_mutables_case(9, 8);
     }
 
     #[test]
     #[should_panic]
     fn test_extract_two_mutables_same() {
-        test_extract_two_mutables_case(5,5);
+        test_extract_two_mutables_case(5, 5);
     }
 
-    fn test_extract_two_mutables_case(first:u32, second: u32) {
-        let mut vec_test = vec![0,1,2,3,4,5,6,7,8,9];
+    fn test_extract_two_mutables_case(first: u32, second: u32) {
+        let mut vec_test = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
         let (a, b) = extract_two_mutable_elements(&mut vec_test, first as usize, second as usize);
 
@@ -1176,37 +1190,143 @@ mod tests {
 
     #[test]
     fn klondike_card_movements() {
-        let mut piles = vec![Pile::new(), Pile::new(), Pile::new()];
-        let mut mover = TestCardMover::new(1, true);
+        let (piles, foundations, deck) = prepare_card_movement_test();
+        let origin_str = format!("{:p}", &foundations[0]);
+        let destination_str = format!("{:p}", &foundations[1]);
+        check_card_movement(
+            piles,
+            foundations,
+            deck,
+            origin_str,
+            destination_str,
+            CardHolder::FOUNDATION(0),
+            CardHolder::FOUNDATION(1),
+            1,
+            true,
+        );
 
-        unsafe {
-            let ptr = &mut piles as *mut Vec<Pile>;
-            let piles2 = &*ptr;
-            mover.origin = Some(&piles2[0]);
-            mover.destination = Some(&piles2[1]);
-        }
+        let (piles, foundations, deck) = prepare_card_movement_test();
+        let origin_str = format!("{:p}", &foundations[0]);
+        let destination_str = format!("{:p}", &piles[1]);
+        check_card_movement(
+            piles,
+            foundations,
+            deck,
+            origin_str,
+            destination_str,
+            CardHolder::FOUNDATION(0),
+            CardHolder::PILE(1),
+            1,
+            false,
+        );
+
+        let (piles, foundations, deck) = prepare_card_movement_test();
+        let origin_str = format!("{:p}", &piles[2]);
+        let destination_str = format!("{:p}", &piles[1]);
+        check_card_movement(
+            piles,
+            foundations,
+            deck,
+            origin_str,
+            destination_str,
+            CardHolder::PILE(2),
+            CardHolder::PILE(1),
+            5,
+            false,
+        );
+
+        let (piles, foundations, deck) = prepare_card_movement_test();
+        let origin_str = format!("{:p}", &piles[2]);
+        let destination_str = format!("{:p}", &foundations[1]);
+        check_card_movement(
+            piles,
+            foundations,
+            deck,
+            origin_str,
+            destination_str,
+            CardHolder::PILE(2),
+            CardHolder::FOUNDATION(1),
+            5,
+            true,
+        );
+
+        let (piles, foundations, deck) = prepare_card_movement_test();
+        let origin_str = format!("{:p}", &*deck);
+        let destination_str = format!("{:p}", &foundations[1]);
+        check_card_movement(
+            piles,
+            foundations,
+            deck,
+            origin_str,
+            destination_str,
+            CardHolder::DECK,
+            CardHolder::FOUNDATION(1),
+            5,
+            true,
+        );
+
+        let (piles, foundations, deck) = prepare_card_movement_test();
+        let origin_str = format!("{:p}", &*deck);
+        let destination_str = format!("{:p}", &piles[1]);
+        check_card_movement(
+            piles,
+            foundations,
+            deck,
+            origin_str,
+            destination_str,
+            CardHolder::DECK,
+            CardHolder::PILE(1),
+            5,
+            true,
+        );
+    }
+
+    fn prepare_card_movement_test() -> (Vec<Pile>, Vec<Foundation>, Box<Deck>) {
+        (
+            vec![Pile::new(), Pile::new(), Pile::new()],
+            vec![
+                Foundation::new(generate_descending_alt_color_starting(0, 1)),
+                Foundation::new(generate_descending_alt_color_starting(1, 1)),
+                Foundation::new(generate_descending_alt_color_starting(2, 1)),
+            ],
+            Box::new(Deck::init(&Vec::new())),
+        )
+    }
+
+    fn check_card_movement(
+        piles: Vec<Pile>,
+        foundations: Vec<Foundation>,
+        deck: Box<Deck>,
+        origin_str: String,
+        destination_str: String,
+        origin: CardHolder,
+        destination: CardHolder,
+        number: u32,
+        result: bool,
+    ) {
+        let mut mover = TestCardMover::new(number as usize, result, origin_str, destination_str);
 
         let mut klondike = Klondike {
-            foundations: Vec::new(),
+            foundations,
             piles,
-            deck:Deck::init(&Vec::new()),
-            mover :&mut mover
+            deck,
+            mover: &mut mover,
         };
 
-        let res = klondike.move_cards(CardHolder::PILE(0), CardHolder::PILE(1), 1);
-        assert_eq!(res, true);
+        let res = klondike.move_cards(origin, destination, number);
+        assert_eq!(res, result);
         assert_eq!(mover.call_count, 1);
     }
 
-    struct TestCardMover<'a, 'b> {
-        origin: Option<&'a (dyn CardOrigin + 'a)>,
-        destination: Option<&'b (dyn CardDestination + 'b)>,
+    struct TestCardMover {
+        origin: String,
+        destination: String,
         card_number: usize,
         to_return: bool,
         call_count: u32,
     }
 
-    impl<'a, 'b> CardMover for TestCardMover<'a, 'b> {
+    impl CardMover for TestCardMover {
         fn move_cards(
             &mut self,
             origin: &mut dyn CardOrigin,
@@ -1214,23 +1334,23 @@ mod tests {
             number: usize,
         ) -> bool {
             self.call_count = self.call_count + 1;
-            if let Some(expected) = self.origin {
-                // convert pointer to text, as direct compare does not work :(
-                assert_eq!(format!("{:p}", origin), format!("{:p}", expected));
-            }
-            if let Some(expected) = self.destination {
-                assert_eq!(format!("{:p}", destination), format!("{:p}", expected));
-            }
+            assert_eq!(format!("{:p}", origin), self.origin);
+            assert_eq!(format!("{:p}", destination), self.destination);
             assert_eq!(self.card_number, number);
             self.to_return
         }
     }
 
-    impl<'a, 'b> TestCardMover<'a, 'b> {
-        pub fn new(card_number: usize, to_return: bool) -> TestCardMover<'a, 'b> {
+    impl TestCardMover {
+        pub fn new(
+            card_number: usize,
+            to_return: bool,
+            origin: String,
+            destination: String,
+        ) -> TestCardMover {
             TestCardMover {
-                origin: None,
-                destination: None,
+                origin,
+                destination,
                 card_number,
                 to_return,
                 call_count: 0,
